e452ae5cbcca60816277efca8d32a2a880bdd823
AMARJITH V <amarjith.v@bridge-global.com>
2022-07-13 15:11:11 +0530

    changes added

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <li v-for="post of posts" :key="post.slug">
-      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
-    </li>
-  </div>
+  <b-container class="mt-3">
+    <div v-for="post of posts" :key="post.slug" class="mt-2">
+      <b-card bg-variant="dark" text-variant="white" :title="post.title">
+        <b-card-text>
+          {{ post.body.children[0].children[0].value }}
+        </b-card-text>
+        <b-button href="#" variant="primary">view</b-button>
+      </b-card>
+    </div>
+  </b-container>
 </template>
 
 <script>
@@ -17,3 +22,5 @@ export default {
   },
 };
 </script>
+<style scoped>
+</style>
